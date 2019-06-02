c0dee448ca2a6580292b6c5c88b9dec94bf08873
Mathuba Dlamini <Mathuba58@gmail.com>
2019-06-02 17:06:29 +0200

    Added function to build the undirected graph

--- connected_components.py
+++ connected_components.py
@@ -2,6 +2,16 @@
 
 import sys
 
+def add_edge(graph, vertex1, vertex2):
+    edges = (vertex1, vertex2)
+    (v1, v2) = (edges)
+    if v1 in graph:
+        if v2 not in graph[v1]:
+            graph[v1].append(v2)
+            graph[v2].append(v1)
+    else:
+        graph[v1].append(v2)
+        graph[v2].append(v1)
 
 def number_of_components(adj):
     result = 0
@@ -9,13 +19,11 @@ def number_of_components(adj):
     return result
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
-    n, m = data[0:2]
-    data = data[2:]
-    edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
-    adj = [[] for _ in range(n)]
-    for (a, b) in edges:
-        adj[a - 1].append(b - 1)
-        adj[b - 1].append(a - 1)
-    print(number_of_components(adj))
+    n, m = map(int, sys.stdin.readline().split())
+    graph = {vertex: [] for vertex in range(1, n+1)}
+
+    for _ in range(m):
+        vert1, vert2 = map(int, sys.stdin.readline().split())
+        add_edge(graph, vert1, vert2)
+    
+    print(number_of_components(graph))
